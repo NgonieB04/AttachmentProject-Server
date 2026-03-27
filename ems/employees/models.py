51a64dc7dae1437bb80f76eac3353b037cdc3638
NgonieB04 <167550926+NgonieB04@users.noreply.github.com>
2026-03-27 15:37:36 +0200

    new files update

--- ems/employees/models.py
+++ ems/employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+    
--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'projects',
     'notifications',    
 ]
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
